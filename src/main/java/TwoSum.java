d33f358d662125eb7fc4ade1b72ce3f4b7b189e6
Shashwath Kattepura Jayabheema Rao <shashwath.basics@gmail.com>
2019-01-01 19:10:16 -0800

    twoSum

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -2,16 +2,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numMap = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            numMap.put(nums[i], i);
+        int i = 0;
+        for (int num : nums) {
+            numMap.put(num, i);
+            i++;
         }
-        for(int j=0; j<nums.length; j++) {
-            if (numMap.containsKey(target - nums[j])){
-                return new int[]{j, numMap.get(target-nums[j])};
+
+        i = 0;
+        for (int num: nums) {
+            if(numMap.containsKey(target - num) &&
+                    i != numMap.get(target-num)) {
+                return new int[]{i, numMap.get(target-num)};
             }
+            i++;
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[]{3, 2, 4}, 6));
     }
 }
